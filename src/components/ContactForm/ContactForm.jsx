d40b886f9d66426933fbafd1f1423ce1024ddc1b
mika <dominikanajda@gmail.com>
2023-08-27 13:07:02 +0200

    form fixes

--- src/components/ContactForm/ContactForm.jsx
+++ src/components/ContactForm/ContactForm.jsx
@@ -17,11 +17,15 @@ export class ContactForm extends Component {
     event.preventDefault();
     const { name, number } = this.state;
     this.props.onSubmit({ name, number });
-    this.ContactForm.reset();
+    this.resetForm();
   };
 
+  resetForm() {
+    this.setState({ name: '', number: '' });
+  }
+
   render() {
-    // const { name, number } = this.state;
+    const { name, number } = this.state;
     return (
       <div className={css.conctactForm}>
         <form onSubmit={this.handleSubmit}>
@@ -33,6 +37,7 @@ export class ContactForm extends Component {
               pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
               title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
               required
+              value={name}
               onChange={this.handleChange}
             />
           </label>
@@ -44,6 +49,7 @@ export class ContactForm extends Component {
               pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
               title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
               required
+              value={number}
               onChange={this.handleChange}
             />
           </label>
@@ -59,4 +65,6 @@ export class ContactForm extends Component {
 ContactForm.propTypes = {
   handleSubmit: PropTypes.func,
   handleChange: PropTypes.func,
+  name: PropTypes.string,
+  number: PropTypes.string,
 };
